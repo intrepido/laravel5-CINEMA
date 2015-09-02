eab4de5a906a975d684c330f88f20d4c77a74f19
Fidel Santana <in3pi287@gmail.com>
2015-09-02 18:20:57 -0500

    Curso de Laravel 5.1, Crear con AJAX

---
+++ app/Http/Controllers/GeneroController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Cinema\Http\Controllers;
+
+use Cinema\Genre;
+use Illuminate\Http\Request;
+
+use Cinema\Http\Requests;
+use Cinema\Http\Controllers\Controller;
+
+class GeneroController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return view('genero.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        if($request->ajax()){
+            Genre::create($request->all());
+            return response()->json([
+               'mensaje' => 'creado'
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -103,6 +103,7 @@
 
 
 <script src='{{asset('js/jquery.min.js')}}'></script>
+{!! Html::script('js/script.js') !!} {{--Otra forma de ponerlo tambien--}}
 <script src='{{asset('js/bootstrap.min.js')}}'></script>
 <script src='{{asset('js/metisMenu.min.js')}}'></script>
 <script src='{{asset('js/sb-admin-2.js')}}'></script>
